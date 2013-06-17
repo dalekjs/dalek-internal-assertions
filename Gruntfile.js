f94e04814816b1f30a1dd8539e2deb3818597f88
asciidisco <public@asciidisco.com>
2013-06-17 19:00:25 +0200

    Adding more CI goodness

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,20 +3,42 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
+    // linting
     jshint: {
       options: {
         jshintrc: '.jshintrc'
       },
-      all: ['Gruntfile.js', 'test/*.js', 'index.js']
+      all: ['Gruntfile.js', 'index.js', 'test/*.js']
     },
 
-    watch: {
-      all: ['Gruntfile.js', 'test/*.js', 'index.js']
+    // testing
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['test/*.js']
+      }
     },
+
+    // code metrics
+    complexity: {
+      generic: {
+        src: ['Gruntfile.js', 'index.js', 'test/*.js'],
+        options: {
+          cyclomatic: 4,
+          halstead: 20,
+          maintainability: 90
+        }
+      }
+    }
+
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-complexity');
 
   grunt.registerTask('lint', 'jshint');
-  grunt.registerTask('test', 'lint');
+  grunt.registerTask('test', ['lint', 'mochaTest', 'complexity']);
 };
