c8974b64d32abf3fa7d635ab30c76afd92f326ea
asciidisco <public@asciidisco.com>
2013-06-17 18:04:43 +0200

    Added gruntfiles

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      all: ['Gruntfile.js', 'test/*.js', 'index.js']
+    },
+
+    watch: {
+      all: ['Gruntfile.js', 'test/*.js', 'index.js']
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.registerTask('lint', 'jshint');
+  grunt.registerTask('travis', 'lint');
+};
